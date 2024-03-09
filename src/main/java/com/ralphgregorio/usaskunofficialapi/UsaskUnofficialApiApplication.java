b7a879e07d0b91e3903a5ad491198e42886c3208
Ralph Gregorio <ralphlouiegregorio@gmail.com>
2024-03-08 22:48:17 -0600

    Spring boot initializr

---
+++ src/main/java/com/ralphgregorio/usaskunofficialapi/UsaskUnofficialApiApplication.java
@@ -0,0 +1,13 @@
+package com.ralphgregorio.usaskunofficialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsaskUnofficialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsaskUnofficialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ralphgregorio/usaskunofficialapi/UsaskUnofficialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ralphgregorio.usaskunofficialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsaskUnofficialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
